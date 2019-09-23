3b5ac3b89b5d7c183d5e7e0c5f6bc75ff019f014
Junior Pastor PÉREZ MOLINA <44443289+JPASTORPM@users.noreply.github.com>
2019-09-23 14:42:15 -0600

    ac

--- Script/Script_Nutrientes.R
+++ Script/Script_Nutrientes.R
@@ -1,8 +1,9 @@
 #------------------------------------------------
-# Script: HAs
+# Script: Nutrientes
 # Autor: Junior Pastor PÃ©rez-Molina
-# Date: 09-20-2019
+# Date: 09-23-2019
 #------------------------------------------------
+
 
 
 #------------------------------------------------
@@ -16,17 +17,159 @@ cat("\014")     # Remove script in console
 
 
 #------------------------------------------------
-dat<-matrix(c(0.9,0.8,0.7, 0.7,0.6, 0.5, 0.6,0.55,0.5, 0.2, 0.1,0.1),nrow = 3, ncol = 4)
-library(plot3D) #exemple(plot3D)
-nr <- r<- nrow(dat)
-nc <- c<- ncol(dat)
-a<-image2D(z = dat, x = 1:nr, y = 1:nc, lwd = 2, shade = 0.2, rasterImage = TRUE,main = "HAs", clab = "Nivel")
+# Loading database
+#------------------------------------------------
+data<-read.table("Data/datos.txt", sep="\t", dec=".", header = TRUE)
+str(data)
+data$Nutriente2<-as.character(data$Nutriente)
+data$Nutriente2[data$Nutriente=="Control"]<-"1_C"
+data$Nutriente2[data$Nutriente=="N"]<-"2_N"
+data$Nutriente2[data$Nutriente=="P"]<-"3_P"
+data$Nutriente2[data$Nutriente=="NP"]<-"4_NP"
+data<-na.omit(data)
 #------------------------------------------------
 
+
+
+#------------------------------------------------
+# Packages
+#------------------------------------------------
+if(!require(yarrr)){githubinstall("yarrr")}
+if(!require(openxlsx)){githubinstall("openxlsx")}
+if(!require(broom)){githubinstall("broom")}
+#------------------------------------------------
+
+
+
+#------------------------------------------------
+# Analisis estadísticos
+#------------------------------------------------
+fun.table.anova<-function(Name,variable, factor1, factor2){
+  data.anova<-data.frame()
+  model = aov(variable ~ factor1*factor2)
+  summary_model<-summary(model)
+  library(broom)
+  inf<-round(glance(model),3)
+  P.value<-round(as.numeric(inf[5]),3)
+  R2<-round(as.numeric(inf[1]),2)
+  F<-round(as.numeric(inf[4]),1)
+  coef<-data.frame((summary_model[[1]][5]))
+  coef<-na.omit(coef)
+  names(coef)<-c("Factor")
+  Nutrientes<-round(coef[1,], 3)
+  Luz<-round(coef[2,], 3)
+  NutrientesXLuz<-round(coef[3,], 3)
+  P<-data.frame(Nutrientes, Luz, NutrientesXLuz)
+  
+  require(lsmeans)
+  lsm = lsmeans(model, pairwise ~ factor1*factor2, adjust="B")
+  t1<-data.frame(cld(lsm[[1]], alpha=.05, Letters=letters))
+  t2<-data.frame(t1[c(1)],t1[c(2)],t1[c(3)],t1[c(4)],t1[c(8)])
+  mean2<-paste(round(t2$lsmean, 3), round(t2$SE, 3), sep=" ± ")
+  mean1<-paste(t2$factor1, t2$factor2, sep="*")
+  mean3<-paste(mean2,t(t2$".group"), sep=" ")
+  mean<-data.frame(mean1,mean3)  
+  mean<-mean[order(mean$mean1),]  
+  mean<-data.frame(t(mean))
+  names(mean)<-c("C*Alta","C*Baja","N*Alta" , "N*Baja","P*Alta","P*Baja", "NP*Alta" ,"NP*Baja")
+  mean<-mean[-1,]
+  a<-shapiro.test(model$residuals)
+  P_value_Shapiro_Wilk<-a$p.value
+  a<-ifelse(a$p.value>0.05, "Normalidad de residuos", "Residuos asimétricos")
+  
+  data.anova <- rbind(data.anova,data.frame(P_value_Shapiro_Wilk, a, Name, mean, P, F, R2, P.value))
+  ###
+  return(data.anova)
+}
+#------------------------------------------------
+TMS<-fun.table.anova(Name="TMS",variable= data$TMS, factor1= data$Nutriente2, factor2= data$Luz)
+RLT<-fun.table.anova(Name="RLT",variable= data$TMS, factor1= data$Nutriente2, factor2= data$Luz)
+RRT<-fun.table.anova(Name="RRT",variable= data$TMS, factor1= data$Nutriente2, factor2= data$Luz)
+RRV<-fun.table.anova(Name="RRV",variable= data$TMS, factor1= data$Nutriente2, factor2= data$Luz)
+#------------------------------------------------
+
+
+
+#------------------------------------------------
+# Cuadro 1
+#------------------------------------------------
+Cuadro_1<-merge(TMS, RLT, all=TRUE)
+Cuadro_1<-merge(Cuadro_1, RRT, all=TRUE)
+Cuadro_1<-merge(Cuadro_1, RRV, all=TRUE)
+#------------------------------------------------
+write.xlsx(time_course, "Results/Cuadro 1.xlsx",
+           sheetName="Cuadro 1",col.names=TRUE,
+           row.names=FALSE, append=FALSE,
+           showNA=TRUE, password=NULL)
 #------------------------------------------------
 
 
 
 
+#------------------------------------------------
+# Figure
+#------------------------------------------------
+pdf(file = "Results/Fig. 1.pdf",
+    width = 10, height = 10)
+#------------------------------------------------
+par(xpd = FALSE,mfrow=c(2,2),mgp = c(2.5,0.5,0), mar = c(6.5,4,1,1))
+pirateplot(formula = TMS ~ Luz + Nutriente2, data = data, 
+           main = "", xlab = "", ylab = "Peso seco total (mg, ±SE)",
+           ylim=c(0.0,1.6),point.pch=NA, 
+           bar.f.col=c("white", "gray45"),avg.line.fun=mean,plot=TRUE,
+           theme = 4, gl.col = NA,
+           avg.line.col = "gray10",
+           inf.f.col = "gray10",
+           jitter.val = 0.09,
+           avg.line.o = 1,bar.b.o=1,
+           avg.line.lwd=3,
+           inf.lwd=1,
+           inf.method="se")
+par(xpd = TRUE)
+#------------------------------------------------
+pirateplot(formula = RLT ~ Luz + Nutriente2, data = data, 
+           main = "", xlab = "", ylab = "Relación peso seco hojas/total (±SE)",
+           ylim=c(0.0,0.8),point.pch=NA, 
+           bar.f.col=c("white", "gray45"),avg.line.fun=mean,plot=TRUE,
+           theme = 4, gl.col = NA,
+           avg.line.col = "gray10",
+           inf.f.col = "gray10",
+           jitter.val = 0.09,
+           avg.line.o = 1,bar.b.o=1,
+           avg.line.lwd=3,
+           inf.lwd=1,
+           inf.method="se")
+par(xpd = TRUE)
+#------------------------------------------------
+pirateplot(formula = RRT ~ Luz + Nutriente2, data = data, 
+           main = "", xlab = "", ylab = "Relación peso seco raíz/total (±SE)",
+           ylim=c(0.0,0.8),point.pch=NA, 
+           bar.f.col=c("white", "gray45"),avg.line.fun=mean,plot=TRUE,
+           theme = 4, gl.col = NA,
+           avg.line.col = "gray10",
+           inf.f.col = "gray10",
+           jitter.val = 0.09,
+           avg.line.o = 1,bar.b.o=1,
+           avg.line.lwd=3,
+           inf.lwd=1,
+           inf.method="se")
+par(xpd = TRUE)
+#------------------------------------------------
+pirateplot(formula = RRV ~ Luz + Nutriente2, data = data, 
+           main = "", xlab = "", ylab = "Relación peso seco raíz/Vástago (±SE)",
+           ylim=c(0.0,0.6),point.pch=NA, 
+           bar.f.col=c("white", "gray45"),avg.line.fun=mean,plot=TRUE,
+           theme = 4, gl.col = NA,
+           avg.line.col = "gray10",
+           inf.f.col = "gray10",
+           jitter.val = 0.09,
+           avg.line.o = 1,bar.b.o=1,
+           avg.line.lwd=3,
+           inf.lwd=1,
+           inf.method="se")
+par(xpd = TRUE)
+#------------------------------------------------
+dev.off()
+#------------------------------------------------
 
 
